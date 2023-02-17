2181747fee091610905ee546bd8278879930abbd
essisteri <essi.ryynanen@tuni.fi>
2023-02-17 19:18:21 +0200

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="nav-item">
-      <Link to="/">Home</Link>
       <Link to="/onecard">Nosta kortti</Link>
       <Link to="/meanings">Korttien merkitykset</Link>
     </div>
